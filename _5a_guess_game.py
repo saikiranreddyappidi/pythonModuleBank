3c9856b81aade85b03b2b228c2d15fea47d94c72
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2022-09-21 01:24:02 +0530

    Added ranges to the hints

--- _5a_guess_game.py
+++ _5a_guess_game.py
@@ -1,6 +1,26 @@
 from random import randint
 
 
+def ahead(c_guess: int, player_guess: int):
+    diff = player_guess-c_guess
+    if diff >= 500:
+        print(f"{player_guess} is too much higher than my guess.")
+    elif diff >= 250:
+        print(f"{player_guess} is too higher than my guess.")
+    else:
+        print(f"{player_guess} is higher than my guess.")
+
+
+def behind(c_guess: int, player_guess: int):
+    diff = c_guess - player_guess
+    if diff >= 500:
+        print(f"{player_guess} is too much lower than my guess.")
+    elif diff >= 250:
+        print(f"{player_guess} is too lower than my guess.")
+    else:
+        print(f"{player_guess} is lower than my guess.")
+
+
 def response_func():
     print("Enter 'y' to play again, Enter 'n' to quit : ", end="")
     response = input()
@@ -29,10 +49,12 @@ def func():
             print("----------------------------------------------")
             response_func()
         elif n > num:
-            print(f"{n} is higher than my guess.")
+            # print(f"{n} is higher than my guess.")
+            ahead(num, n)
             score -= 10
         elif n < num:
-            print(f"{n} is lower than my guess.")
+            # print(f"{n} is lower than my guess.")
+            behind(num, n)
             score -= 10
 
         if i == 1:
@@ -41,6 +63,7 @@ def func():
             print("You lost the game.")
             print("----------------------------------------------")
             response_func()
+        print(f"{i} chances left.")
 
 
 func()
