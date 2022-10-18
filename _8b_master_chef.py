d079b97a085cc0de04feccda03db8af97c8a13a6
Sai Kiran Reddy Appidi <98457635+saikiranreddyappidi@users.noreply.github.com>
2022-10-19 01:06:57 +0530

    Update _8b_master_chef.py

--- _8b_master_chef.py
+++ _8b_master_chef.py
@@ -1,3 +1,7 @@
-w = int(input("Enter no of weeks: "))
-cost = int(input("Enter cost per week: "))
+try:
+	w = int(input("Enter no of weeks: "))
+	cost = int(input("Enter cost per week: "))
+except:
+	print("Enter a valid input")
+	exit(1)
 print('Total amount to be paid: ', w*cost)
